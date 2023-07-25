d464fb987251d0a391c445a86f3ce41a0ddbd8a7
djanyreason <djanyreason@gmail.com>
2023-07-25 16:40:10 -0400

    Completes ex. 3.8, adds POST data to morgan log
    This commit adds to the server logging the body data sent with a POST
    request to the end of the log. This is done by adding a custom token
    to morgan and adding it to the log. This also requires replacing the
    'tiny' parameter with the equivalent token string.
    
    No additional information is logged when a the request is anything
    other than POST.

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,7 +3,12 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('postData', (req, res) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : " ";
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
 let entries = [
   { 
