71dce122d01de05bf737fd2939332f5997258c23
djanyreason <djanyreason@gmail.com>
2023-07-27 12:11:10 -0400

    Completes ex. 3.10, deploying backend to fly.io
    This commit adds fly.io deployment to the backend of the phonebook.
    The deployed backend responds appropriately to GET requests. It does
    not handle DELETE or PUSH requests, instead returning a 301 response.
    Given where this appears in the course material, and looking at
    future part 3 exercises, I believe this is the expected state of the
    initial backend deployment.

---
+++ phonebook/README.md
@@ -0,0 +1,3 @@
+Phonebook project for pt. 3 of Full Stack Open
+
+Deployed project available at: https://fso03-phonebook.fly.dev/
--- phonebook/index.js
+++ phonebook/index.js
@@ -81,7 +81,7 @@ app.post('/api/persons', (request, response) => {
   response.json(entry);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
